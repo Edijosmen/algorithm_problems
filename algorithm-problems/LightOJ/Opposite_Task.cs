5d4a59628189cf3b89576e85019b4e95d5dd982c
Edijosmen <naruto-capitulo1@hotmail.com>
2023-01-25 21:35:19 -0500

    Opposite Task

---
+++ algorithm-problems/LightOJ/Opposite_Task.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algorithm_problems.LightOJ
+{
+    public class Opposite_Task
+    {
+        public void Run_Opposite_Task()
+        {
+            int T = int.Parse(Console.ReadLine());
+            for (int i = 0; i < T; i++)
+            {
+                int n = int.Parse(Console.ReadLine());
+                if (n > 10) Console.WriteLine($"10 {n - 10}");
+                else
+                    Console.WriteLine($"0 {n}");
+            }
+        }
+    }
+}
---
+++ algorithm-problems/Program.cs
@@ -0,0 +1,4 @@
+﻿using algorithm_problems.LightOJ;
+
+var pro = new Opposite_Task();
+pro.Run_Opposite_Task();
