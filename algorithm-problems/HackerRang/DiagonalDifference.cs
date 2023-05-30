8070c87e9c3ad5371ca8f7a5fd194574a25fc852
Edijosmen <naruto-capitulo1@hotmail.com>
2023-05-30 17:55:41 -0500

    hackerRank-Diagonal-Diference

---
+++ algorithm-problems/HackerRang/DiagonalDifference.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algorithm_problems.HackerRang
+{
+    public  class DiagonalDifference
+    {
+        public static void Solutions()
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<List<int>> arr = new List<List<int>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList());
+            }
+
+            int result = DiagonalDifferencevalor(arr);
+            Console.WriteLine(result);
+        }
+
+        private static int DiagonalDifferencevalor(List<List<int>> arr)
+        {
+            List<int> l1 = new List<int>();
+            List<int> l2 = new List<int>();
+            int arry = arr.Count;
+            int dato1 = 0;
+            int dato2 = 0;
+           for(int i=0;i< arr.Count; i++)
+            {
+                List<int> fl = arr[i];
+                l1.Add(fl[i]);
+                dato1 += fl[i];
+                l2.Add(fl[arry-1]);
+                dato2 += fl[arry-1];
+                arry += -1;
+            }
+            var mayor = Math.Max(dato1, dato2);
+            var menor = Math.Min(dato1, dato2);
+            return mayor - menor;
+        }
+    } 
+}
--- algorithm-problems/Program.cs
+++ algorithm-problems/Program.cs
@@ -1,4 +1,42 @@
-﻿using algorithm_problems.LightOJ;
+﻿//using algorithm_problems.LightOJ;
 
-var pro = new Opposite_Task();
-pro.Run_Opposite_Task();
+//var pro = new Opposite_Task();
+//pro.Run_Opposite_Task();
+using algorithm_problems.HackerRang;
+
+DiagonalDifference.Solutions();
+//GrandeBook book = new GrandeBook();
+//book.AddGrade(44.2F);
+//book.AddGrade(54.2F);
+
+//GrandeBook book2 = book;
+//book2.AddGrade(78.6F);
+//book2.AddGrade(78.6F);
+//string raza = "dog";
+//Console.WriteLine(raza);
+//List<string> list = new List<string>();
+//list.Add("hola1");
+//list.Add("hola2");
+//list.Add("hola3");
+//(int, string) tq = (2, "uno");
+//raza = "perros";
+
+//Console.WriteLine(raza);
+
+//Console.WriteLine(tq.Item1);
+//foreach (string item in list)
+//{
+
+//    Console.WriteLine(item);
+//}
+
+
+
+public class GrandeBook{
+
+    List<float> list = new();
+    protected void AddGrade( float number)
+    {
+        list.Add(number);
+    }
+}
